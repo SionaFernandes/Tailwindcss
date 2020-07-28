f9e23e7756db2d76b7277bba8b18dc468e7833e6
Siona Fernandes <sionac.fernandes@gmail.com>
2020-07-28 17:18:26 +0530

    header added

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+let hambuger = document.getElementById("hambuger");
+let hambugerdrop = document.getElementById("hambuger-drop");
+console.log(hambuger, hambugerdrop);
+
+hambuger.addEventListener("click", function () {
+  //  hambuger.classList.toggle("show");
+  if (!hambuger.classList.contains("show")) {
+    hambuger.classList.add("show");
+    hambugerdrop.style.display = "block";
+  } else {
+    hambuger.classList.remove("show");
+    if (hambugerdrop.style.display == "block") {
+      // if is menuBox displayed, hide it
+      hambugerdrop.style.display = "none";
+    }
+  }
+});
